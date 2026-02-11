56c350d4b747e6f81abdcf9b72d6c0797425ad96
Alex-Cristian <craciunescua5@gmail.com>
2026-02-11 19:05:53 +0200

    add req.txt

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-o-lxwole_@_9)d+@2jg#yo(#j#so8jnql84d=2g$gh0$docki+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
